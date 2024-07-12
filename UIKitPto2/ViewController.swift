253fac13dc02571d6a43deff41295c1da077f2c9
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-07-12 16:29:07 -0700

    Initial Commit

---
+++ UIKitPto2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitPto2
+//
+//  Created by Sothesom on 22/04/1403.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
